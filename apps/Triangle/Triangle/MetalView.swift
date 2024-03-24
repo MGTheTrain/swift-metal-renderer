81757785ab4749786194dfe070bca898cc029f00
MGTheTrain <marvin.gajek1@web.de>
2024-03-24 17:10:28 +0100

    [improvement] - utilise self keyword

--- apps/Triangle/Triangle/MetalView.swift
+++ apps/Triangle/Triangle/MetalView.swift
@@ -37,7 +37,7 @@ class MetalView: UIView {
     */
     override init(frame: CGRect) {
         super.init(frame: frame)
-        commonInit()
+        self.commonInit()
     }
     
     /*
@@ -48,7 +48,7 @@ class MetalView: UIView {
      */
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        commonInit()
+        self.commonInit()
     }
     
     /*
@@ -56,9 +56,9 @@ class MetalView: UIView {
      @abstract Initializes Metal-related components.
     */
     private func commonInit() {
-        buildDevice()
-        buildVertexBuffers()
-        buildPipeline()
+        self.buildDevice()
+        self.buildVertexBuffers()
+        self.buildPipeline()
     }
     
     /*
@@ -68,11 +68,11 @@ class MetalView: UIView {
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         if let superview = superview {
-            displayLink = CADisplayLink(target: self, selector: #selector(displayLinkDidFire(_:)))
-            displayLink?.add(to: .main, forMode: .common)
+            self.displayLink = CADisplayLink(target: self, selector: #selector(displayLinkDidFire(_:)))
+            self.displayLink?.add(to: .main, forMode: .common)
         } else {
-            displayLink?.invalidate()
-            displayLink = nil
+            self.displayLink?.invalidate()
+            self.displayLink = nil
         }
     }
     
@@ -81,11 +81,11 @@ class MetalView: UIView {
      @abstract Sets up Metal device.
     */
     private func buildDevice() {
-        device = MTLCreateSystemDefaultDevice()
-        metalLayer = layer as? CAMetalLayer
-        metalLayer.device = device
-        metalLayer.pixelFormat = .bgra8Unorm
-        metalLayer.contentsScale = UIScreen.main.scale
+        self.device = MTLCreateSystemDefaultDevice()
+        self.metalLayer = layer as? CAMetalLayer
+        self.metalLayer.device = self.device
+        self.metalLayer.pixelFormat = .bgra8Unorm
+        self.metalLayer.contentsScale = UIScreen.main.scale
     }
     
     /*
@@ -105,12 +105,12 @@ class MetalView: UIView {
         pipelineDescriptor.fragmentFunction = fragmentFunc
         
         do {
-            pipeline = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+            self.pipeline = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
         } catch {
             print("Error occurred when creating render pipeline state: \(error)")
         }
         
-        commandQueue = device.makeCommandQueue()
+        self.commandQueue = device.makeCommandQueue()
     }
     
     /*
@@ -124,7 +124,7 @@ class MetalView: UIView {
             0.5, -0.5, 0, 1
         ]
         
-        positionBuffer = device.makeBuffer(bytes: positions, length: MemoryLayout<Float>.size * positions.count, options: [])
+        self.positionBuffer = device.makeBuffer(bytes: positions, length: MemoryLayout<Float>.size * positions.count, options: [])
     }
     
     /*
@@ -133,7 +133,7 @@ class MetalView: UIView {
      @param displayLink The CADisplayLink object associated with the event.
     */
     @objc private func displayLinkDidFire(_ displayLink: CADisplayLink) {
-        redraw()
+        self.redraw()
     }
     
     /*
@@ -141,7 +141,7 @@ class MetalView: UIView {
      @abstract Redraws the Metal content.
     */
     private func redraw() {
-        guard let drawable = metalLayer.nextDrawable() as? CAMetalDrawable else {
+        guard let drawable = self.metalLayer.nextDrawable() as? CAMetalDrawable else {
             return
         }
         
