b00a07641e512d37d2f9577b2bd196ad5e5a0501
MGTheTrain <marvin.gajek1@web.de>
2024-03-24 21:11:36 +0100

    [improvement] - initial setup of MetalViewer iOS app

---
+++ apps/MetalViewer/MetalViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetalViewer
+//
+//  Created by Marvin Gajek on 24.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
