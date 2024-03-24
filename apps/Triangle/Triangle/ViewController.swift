78aaaa2ebf687dd0c4a9093ef0885ef9d0fd761d
MGTheTrain <marvin.gajek1@web.de>
2024-03-24 11:31:43 +0100

    [improvement] - create initial iOS Swift metal triangle app

---
+++ apps/Triangle/Triangle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Triangle
+//
+//  Created by Marvin Gajek on 24.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
